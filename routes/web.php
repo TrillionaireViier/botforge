ea6912bf80e4e2f3a0d3ceb935151a5141e687dc
Danila Vier <danilavier@MacBook-Pro-Danila.local>
2026-08-11 01:01:05 +0300

    Add test route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\AuthController;
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
+Route::get('/test-deploy', function () {
+    return 'deploy-success-123';
+});
+
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
